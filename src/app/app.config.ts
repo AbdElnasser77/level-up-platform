f71d78ea4323f7884c64992536b311282bb11fc7
AbdElnasser77 <a7med3bnaser77@gmail.com>
2026-08-16 12:08:08 +0300

    configured the tailwind and primeng css layers

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,22 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import Aura from '@primeuix/themes/aura';
 
 import { routes } from './app.routes';
+import { providePrimeNG } from 'primeng/config';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
-  ]
+    provideRouter(routes),
+    providePrimeNG({
+      theme: {
+        preset: Aura,
+        options: {
+          darkModeSelector: '.dark',
+          cssLayer: { name: 'primeng', order: 'tailwind-base, primeng, tailwind-utilities' },
+        },
+      },
+    }),
+  ],
 };
